de9a9d4c7ed95ceb0b34f97dd1e4220328ec82d2
Miné E <mine_esen@yahoo.ca>
2019-05-31 20:02:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,6 +2,6 @@ def line(customers)
   if customers.length == 0
     puts "The line is currently empty."
   else
-    puts katz_deli.length
+    customers.each_with_index { |customer, index| puts "The line is currently: #{index +1}. #{customer}" }
   end
 end
